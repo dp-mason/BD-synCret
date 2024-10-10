48fa25793fb1fe937c20de73eda35d4cfb9fcbec
dp-mason <davidpattenmason@gmail.com>
2024-10-10 00:09:26 -0600

    zig plugin is functional

--- templates/zig_template/src/main.zig
+++ templates/zig_template/src/main.zig
@@ -12,12 +12,13 @@ const OUTBUF_SAMPLES = 256;
 
 // ComputeSaw calculates the amplitude of a saw wave at a given time
 fn ComputeSaw(timeElapsed: f32, freq: f32, lfoOne: f32, lfoTwo: f32) f32 {
+    _ = lfoTwo;
     // Frequency modulation before phase calculation
     const result = math.modf(PHASE + (freq * timeElapsed));
     PHASE = result.fpart;
 
     // Calculate amplitude (-10 to 10) and modulate with lfoOne
-    const amplitude = (PHASE - 0.5) * -2.0 * lfoOne * lfoTwo;
+    const amplitude = (PHASE - 0.5) * -2.0 * lfoOne;
     return amplitude;
 }
 
@@ -39,6 +40,8 @@ pub fn batchComputeWf() !void {
         const res_bytes:[4]u8 = @bitCast(ComputeSaw(sampleTime, freqHz, lfoOne, lfoTwo));
         @memcpy(output[(i*4)..(i*4+4)], &res_bytes);
     }
+
+    plugin.output(&output);
     
     return;
 }
