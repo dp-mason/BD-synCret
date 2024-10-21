b0a0beda466942f7b5939b4f89c8ca03cb5a8aee
dp-mason <davidpattenmason@gmail.com>
2024-10-20 20:02:29 -0600

    ask before installing each PDK

--- get_prereqs.sh
+++ get_prereqs.sh
@@ -6,9 +6,44 @@ cd ..;
 
 curl https://get.extism.org/cli | sh;
 
-# Rust Prereqs
-sudo apt install pkg-config && \
-rustup target add wasm32-unknown-unknown;
+read -p "Would you like to install the Rust PDK? (y/n) " response
+# Convert the response to lowercase
+response=$(echo "$response" | tr '[:upper:]' '[:lower:]')
+if [[ "$response" == "y" || "$response" == "yes" ]]; then
+    echo "Install Rust PDK..."
+    # Rust Prereqs
+    sudo apt install pkg-config && \
+    rustup target add wasm32-unknown-unknown;
+fi
 
-# JS Prereqs
-curl https://raw.githubusercontent.com/extism/js-pdk/main/install.sh | sh;
+read -p "Would you like to install the JavaScript PDK? (y/n) " response
+# Convert the response to lowercase
+response=$(echo "$response" | tr '[:upper:]' '[:lower:]')
+if [[ "$response" == "y" || "$response" == "yes" ]]; then
+    # JS Prereqs
+    curl https://raw.githubusercontent.com/extism/js-pdk/main/install.sh | sh;
+fi
+
+read -p "Would you like to install the Go PDK? (y/n) " response
+# Convert the response to lowercase
+response=$(echo "$response" | tr '[:upper:]' '[:lower:]')
+if [[ "$response" == "y" || "$response" == "yes" ]]; then
+    # Go Prereqs
+    echo "TODO"
+fi
+
+read -p "Would you like to install the C Language PDK? (y/n) " response
+# Convert the response to lowercase
+response=$(echo "$response" | tr '[:upper:]' '[:lower:]')
+if [[ "$response" == "y" || "$response" == "yes" ]]; then
+    # C Prereqs
+    echo "TODO"
+fi
+
+read -p "Would you like to install the Zig PDK? (y/n) " response
+# Convert the response to lowercase
+response=$(echo "$response" | tr '[:upper:]' '[:lower:]')
+if [[ "$response" == "y" || "$response" == "yes" ]]; then
+    # Zig Prereqs
+    echo "TODO"
+fi
